c8d85b97fd77c1e158c53d46d71316dd62fbf118
utime <utime@pointpark.com>
2023-02-09 10:53:44 +0900

    comment add.

--- wordparse/src/main/java/com/utime/msword/define/EDocPictureType.java
+++ wordparse/src/main/java/com/utime/msword/define/EDocPictureType.java
@@ -2,6 +2,7 @@ package com.utime.msword.define;
 
 /**
  * Picture type
+ * @see https://poi.apache.org/apidocs/dev/index.html?org/apache/poi/xwpf/usermodel/XWPFPicture.html
  */
 public enum EDocPictureType {
     EMF,
